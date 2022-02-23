f0cd53907ffa3f2e6e92f10db2c3330ab5fba673
Jan Franz Kammellander <janschwiderik@gmail.com>
2022-02-23 15:40:44 +0100

    Edited🚧: the attribute is now the arbitrary precision and not the number of the root

--- src/methods/SquareRoot.java
+++ src/methods/SquareRoot.java
@@ -1,12 +1,12 @@
 package methods;
 
 public class SquareRoot implements Calculation{
-    // Declaring a private variable called `root` that is of type `long`.
-    private long root;
+    // Making the precision variable private and final.
+    private final double precision;
 
-    // This is the constructor of the class. It is used to initialize the class.
-    public SquareRoot(long n) {
-        this.root = n;
+    // This is a constructor. It is used to initialize the object.
+    public SquareRoot(double precision) {
+        this.precision = precision;
     }
 
     /**
@@ -14,12 +14,11 @@ public class SquareRoot implements Calculation{
      */
     @Override
     public void execute() {
-        float a = this.root;
-        float b = 1;
-        double e = 0.000001;
-        while (a - b > e) {
+        double a = 1000;
+        double b = 1;
+        while (a - b > precision) {
             a = (a + b) / 2;
-            b = this.root / a;
+            b = this.precision / a;
         }
     }
 }
